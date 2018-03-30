9d3f806b5343653dec8bf86df14dd354ae506d43
Adam Hotaling <Adam.Hotaling@tufts.edu>
2018-03-30 14:54:16 -0500

    Typo fix

--- conf.py
+++ conf.py
@@ -87,7 +87,9 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'logo': 'lc-logo.svg'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
